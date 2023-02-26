aea50ef68f032338d211e1838fd1e5d4b0a80c20
alejo0998 <alejobusso09@gmail.com>
2023-02-26 20:53:06 -0300

    Requerimiento de que cuando un usuario ingresa al admin, solo ve sus respuestas, para que pueda entrar cualquier usuario al admin debe ser staff, los superusuarios ven todas las respuestas

--- challenge_monet/settings.py
+++ challenge_monet/settings.py
@@ -104,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
